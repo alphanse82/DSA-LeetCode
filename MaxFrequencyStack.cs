ea15bb08038a582e8ff54966b33cab88a6022578
alphanse82 <RakeshKumarGupta@outlook.com>
2021-05-20 23:07:57 -0700

    Create MaxFrequencyStack.cs

---
+++ MaxFrequencyStack.cs
@@ -0,0 +1,73 @@
+public class CustomStack {
+
+    int maxFrequencyCount = 0; 
+    
+    Dictionary<int,int> frequencyCounter = new Dictionary<int,int>();
+    Dictionary<int,Stack<int>> stack = new Dictionary<int,Stack<int>>();
+    
+    public FreqStack() {
+        maxFrequencyCount = 1;
+        frequencyCounter = new Dictionary<int,int>();
+        stack = new Dictionary<int,Stack<int>>();
+    }
+    
+    public void Push(int val) {
+        
+        int currentItemFrequency = 0;
+        
+        if( !frequencyCounter.ContainsKey(val) ){
+            frequencyCounter.Add(val, 1);
+            currentItemFrequency = 1;
+            
+        }
+        else {
+            frequencyCounter[val] = frequencyCounter[val] + 1;
+            currentItemFrequency = frequencyCounter[val];
+        }
+        
+        if( currentItemFrequency > maxFrequencyCount  ) {
+                
+            maxFrequencyCount = currentItemFrequency;
+        }
+        
+        if( !stack.ContainsKey(currentItemFrequency) )
+                stack.Add(currentItemFrequency, new Stack<int>());
+
+        stack[currentItemFrequency].Push(val);
+        
+        // Print($"push({val})");
+    }
+    
+    public int Pop() {
+        
+        int item = stack[maxFrequencyCount].Pop();
+        
+        frequencyCounter[item] -= 1;    
+        
+        if( frequencyCounter[item] == 0) {
+            frequencyCounter.Remove(item);
+        }
+        
+        if( stack[maxFrequencyCount].Count == 0 ) {
+            stack.Remove(maxFrequencyCount);
+            maxFrequencyCount--;
+        }
+        
+        // Print($"pop({item})");
+        
+        return item;
+        
+    }
+    
+    private void Print(string action) {
+        
+        string str = $"{action} Max = {maxFrequencyCount} , ";
+        Console.WriteLine(str);
+        foreach(KeyValuePair<int,Stack<int>> kv in stack) {
+            str = $" Index => {kv.Key} , CountOfItems = {kv.Value.Count}";
+            Console.WriteLine(str);
+        }
+        
+        Console.WriteLine("");
+    }
+}
